7b8281fceb9c8ad2e1d3254f1c5e99f8a55b736f
Sahil Nainani <sahilnainani45226@gmail.com>
2023-08-01 17:18:14 +0530

    first commit

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken')
 const userTask = async(req,resp) =>{
     try{
         const {title,subtitle} = req.body
-        console.log("task")
         if(title && subtitle){
 
             const { authorization } = req.headers
@@ -31,4 +30,29 @@ const userTask = async(req,resp) =>{
     }
 }
 
-module.exports = {userTask}
+const getTasks = async(req, resp) =>{
+    try{
+        const pageSize = parseInt(req.query.size)
+        const pageNumber = parseInt(req.query.page)
+
+        const { authorization } = req.headers
+        token = authorization.split(' ')[1]
+        const { userID } = jwt.verify(token,process.env.JWT_SECRET_KEY)
+
+        const skipDocuments = (pageNumber - 1) * pageSize;
+
+        const userTasks = await Task.find({
+            userID:userID
+        },
+        {_id:0,title:1,subtitle:1},
+        { skip: skipDocuments, limit: pageSize })
+
+        console.log(pageSize,pageNumber)
+        console.log(userTasks)
+        resp.send(userTasks)
+    }
+    catch(e){
+        resp.send(e.message)
+    }
+}
+module.exports = {userTask,getTasks}
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 const express =  require('express')
 const { userLogin,userRegistration, defaultPage, changePassword } = require('../controllers/user_controller')
 const { checkUserAuth } = require('../middleware/auth')
-const { userTask } = require('../controllers/task_controller')
+const { userTask, getTasks } = require('../controllers/task_controller')
 
 const router = express.Router()
 
@@ -10,12 +10,15 @@ router.get('/',(req,resp)=>{
     resp.send("TEMP")
 })
 router.use('/changepassword',checkUserAuth)
-router.use('/addtask',userTask)
+router.use('/addtask',checkUserAuth)
+// router.use('/tasks',checkUserAuth)
 
 router.post('/login',userLogin)
 router.post('/register',userRegistration)
 router.post('/changepassword',changePassword)
 router.post('/addtask',userTask)
+
+router.get('/tasks',getTasks)
 
 router.use("*",defaultPage)
 
